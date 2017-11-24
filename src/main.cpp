70f8181ec235c9ca1ee0b6c0f30296b73ff0acdd
Agustin Valiña <agusvali97@gmail.com>
2017-11-24 13:46:19 -0300

    needs case implemetation (interrupts)

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,78 @@
-// ####
-// # Arduino Mega 2560 pin configuration
-// ####
-// # D0  -> RX0                     *
-// # D1  -> TX0                     *
-// # D2  -> Interrupt 0 | PWM       *
-// # D3  -> Interrupt 1 | PWM       *
-// # D4  -> PWM                     *
-// # D5  -> PWM                     *
-// # D6  -> PWM                     *
-// # D7  -> PWM                     *
-// # D8  -> PWM                     *
-// # D9  -> PWM                     *
-// # D10 -> PWM                     *
-// # D11 -> PWM                     *
-// # D12 -> PWM                     *
-// # D13 -> PWM                     *
-// # D14 -> TX3                     *
-// # D15 -> RX3                     *
-// # D16 -> TX2                     *
-// # D17 -> RX2                     *
-// # D18 -> Interrupt 5 | TX1       *
-// # D19 -> Interrupt 4 | RX1       *
-// # D20 -> Interrupt 3 | SDA       *
-// # D21 -> Interrupt 2 | SCL       *
-// # D22 -> Jumper 1
-// # D23 -> Jumper 2
-// # D24 -> Jumper 3
-// # D25 -> Jumper 4
-// # D26 -> (74HC4511) pin 7
-// # D27 -> (74HC4511) pin 1
-// # D28 -> (74HC4511) pin 2
-// # D29 -> (74HC4511) pin 6
-// # D30 -> Decimal point
-// # D31 -> PIR #1
-// # D32 -> PIR #2
-// # D33 -> Ultrasonic trigger
-// # D34 -> Ultrasonic echo
-// # D35 -> Start alarm button
-// # D36 ->                         *
-// # D37 ->                         *
-// # D38 ->                         *
-// # D39 ->                         *
-// # D40 ->                         *
-// # D41 ->                         *
-// # D42 ->                         *
-// # D43 ->                         *
-// # D44 -> PWM                     *
-// # D45 -> PWM                     *
-// # D46 -> PWM                     *
-// # D47 ->                         *
-// # D48 ->                         *
-// # D49 ->                         *
-// # D50 -> MISO                    *
-// # D51 -> MOSI                    *
-// # D52 -> SCK                     *
-// # D53 -> SS                      *
-// ####
-// # A0  ->                         *
-// # A1  ->                         *
-// # A2  ->                         *
-// # A3  ->                         *
-// # A4  ->                         *
-// # A5  ->                         *
-// # A6  ->                         *
-// # A7  ->                         *
-// # A8  ->                         *
-// # A9  ->                         *
-// # A10 ->                         *
-// # A11 ->                         *
-// # A12 ->                         *
-// # A13 ->                         *
-// # A14 ->                         *
-// # A15 ->                         *
-// ####
+/**
+Arduino Mega 2560 pin configuration
+
+D0  -> RX0                     *
+D1  -> TX0                     *
+D2  -> Interrupt 0 | PWM       *
+D3  -> Interrupt 1 | PWM       *
+D4  -> PWM                     *
+D5  -> PWM                     *
+D6  -> PWM                     *
+D7  -> PWM                     *
+D8  -> PWM                     *
+D9  -> PWM                     *
+D10 -> PWM                     *
+D11 -> PWM                     *
+D12 -> PWM                     *
+D13 -> PWM                     *
+D14 -> TX3                     *
+D15 -> RX3                     *
+D16 -> TX2                     *
+D17 -> RX2                     *
+D18 -> Motion sensor 1 | Interrupt 5
+D19 -> Motion sensor 2 | Interrupt 4
+D20 -> Interrupt 3 | SDA       *
+D21 -> Interrupt 2 | SCL       *
+D22 ->                         *
+D23 ->                         *
+D24 ->                         *
+D25 ->                         *
+D26 ->                         *
+D27 ->                         *
+D28 ->                         *
+D29 ->                         *
+D30 ->                         *
+D31 ->                         *
+D32 ->                         *
+D33 -> Ultrasonic trigger
+D34 -> Ultrasonic echo
+D35 -> Start/stop alarm button
+D36 ->                         *
+D37 ->                         *
+D38 ->                         *
+D39 ->                         *
+D40 ->                         *
+D41 ->                         *
+D42 ->                         *
+D43 ->                         *
+D44 -> PWM                     *
+D45 -> PWM                     *
+D46 -> PWM                     *
+D47 ->                         *
+D48 ->                         *
+D49 ->                         *
+D50 -> MISO                    *
+D51 -> MOSI                    *
+D52 -> SCK                     *
+D53 -> SS                      *
+##
+A0  ->                         *
+A1  ->                         *
+A2  ->                         *
+A3  ->                         *
+A4  ->                         *
+A5  ->                         *
+A6  ->                         *
+A7  ->                         *
+A8  ->                         *
+A9  ->                         *
+A10 ->                         *
+A11 ->                         *
+A12 ->                         *
+A13 ->                         *
+A14 ->                         *
+A15 ->                         *
+**/
 
 // Headers
 #include <Arduino.h>
@@ -87,6 +87,9 @@ template <class T, size_t tSize>
 T arraySize (T (&array) [tSize] ) {
     return tSize;
 }
+
+// Macros
+#define MINUTES_TO_MILLIS(x) x*60000
 
 // Flags         12345678
 #define BOARD_1 B00000001 // 1
@@ -111,22 +114,40 @@ T arraySize (T (&array) [tSize] ) {
 
 // Variabes
 int boardSelectionPins [] = {22, 23, 24, 25};
+static int motionSensorPin_1 = 18;
+static int motionSensorPin_2 = 19;
+
+
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+unsigned long motionSensorInterval = MINUTES_TO_MILLIS (5);
 
 bool runCode = false; // by default, just for safe measures, don't run any code
+bool startSensing = false;
+bool alarmActivated = false;
 
 static jmp_buf jmpBuff; // Jmp buffer
 
-byte board;
+byte board = B00000000;
 byte boardList [] = {BOARD_1, BOARD_2, BOARD_3, BOARD_4};
 // Variables
 
 
 // Functions
-// ...
+void motionSensorTriggered (int sensorReference) {
+    // Send mail
+}
+void motionSensorTriggered_1 () {
+    motionSensorTriggered (1);
+}
+void motionSensorTriggered_2 () {
+    motionSensorTriggered (2);
+}
 // Functions
 
 void setup () {
-    board = B00000000; // Blank definition
+    attachInterrupt (digitalPinToInterrupt (motionSensorPin_1), motionSensorTriggered_1, RISING);
+    attachInterrupt (digitalPinToInterrupt (motionSensorPin_2), motionSensorTriggered_2, RISING);
 
     #ifdef DEBUG
     Serial.begin (9600);
@@ -182,6 +203,8 @@ void setup () {
 
 void loop () {
     if (runCode) {
-        // Run code here
+        currentMillis = millis ();
+
+        if (currentMillis - previousMillis >= motionSensorInterval) {}
     }
 }
